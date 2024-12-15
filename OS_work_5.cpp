f0ff45cb11b875235b441515d1e1f27967d8e7a2
xxncxbc <674holland@gmail.com>
2024-12-16 00:54:40 +0300

    final

--- OS_work_5.cpp
+++ OS_work_5.cpp
@@ -1,19 +1,29 @@
 ﻿#include <windows.h>
 #include <iostream>
 #include <vector>
+#include <conio.h>
 using namespace std;
 
 
-int main(){
-    HANDLE ReadPipe1, WritePipe1, ReadPipe2, WritePipe2; 
+int main() {
+    HANDLE ReadPipe1, WritePipe1, ReadPipe2, WritePipe2;
+    HANDLE EndEvent = CreateEvent(NULL, FALSE, FALSE, TEXT("EndEvent"));
+
+
+    SECURITY_ATTRIBUTES saAttr;
+
+    // Настройка безопасности канала
+    saAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
+    saAttr.lpSecurityDescriptor = NULL; //снимаем защиту
+    saAttr.bInheritHandle = TRUE; //разрешаем наследование
 
     // Создание анонимных каналов
-    if (!CreatePipe(&ReadPipe1, &WritePipe1, NULL, 0)) {
+    if (!CreatePipe(&ReadPipe1, &WritePipe1, &saAttr, 0)) {
         cerr << "Error creating channel number 1" << endl;
         return 1;
     }
 
-    if (!CreatePipe(&ReadPipe2, &WritePipe2, NULL, 0)) {
+    if (!CreatePipe(&ReadPipe2, &WritePipe2, &saAttr, 0)) {
         cerr << "Error creating channel number 2" << endl;
         return 1;
     }
@@ -42,7 +52,34 @@ int main(){
 
     //запуск Small
     CreateProcess(NULL, &Arg[0], NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
-    WaitForSingleObject(pi.hThread, INFINITE);
 
+    //передаем размер массива и элементы
+    DWORD bytesWritten, bytesRead;
+
+    WriteFile(WritePipe1, &n, sizeof(n), &bytesWritten, NULL);
+    for (int i = 0; i < n; ++i) {
+        WriteFile(WritePipe1, &arr[i], sizeof(arr[i]), &bytesWritten, NULL);
+    }
+
+    //Получаем новый массив
+
+    int k = 0; 
+    double temp;
+    vector<double> new_arr;
+
+    ReadFile(ReadPipe2, &k, sizeof(k), &bytesRead, NULL);
+    
+    for (int i = 0; i < k; ++i) {
+        ReadFile(ReadPipe2, &temp, sizeof(temp), &bytesWritten, NULL);
+        new_arr.push_back(temp);
+    }
+
+    cout << endl << endl << "Got from Small: " << endl << "Size of new array: " << k << endl << "Elements: " << endl;
+
+    for (double x : new_arr) cout << x << endl;
+
+    cout << endl << "Program is done";
+    _getch();
+    SetEvent(EndEvent);
 }
 
